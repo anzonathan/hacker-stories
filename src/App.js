95c679589dbcdfc847e184773a61ce7a9101f755
Nathaniel Mugenyi <anzonathan@gmail.com>
2024-12-20 19:02:14 +0300

    Add: Objects in page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import * as React from 'react';
 
+const title = 'Test';
+const word = 'hi';
+
+const welcome = {
+  greeting: 'Hi',
+  name: 'Nathaniel',
+}
+
+
 function App() {
+
+
   return (
     <div>
-      <h1>Hello World</h1>
-    </div>
+      <h1>{welcome.greeting} {welcome.name}</h1>
+
+      <label htmlFor="search">search: </label>
+      <input id="search" type="text"/>
+      </div>
   );
 }
 
